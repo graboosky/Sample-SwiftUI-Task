90d96efb31c86e95e727edad7a21d3d097580c22
Patryk Grabowski <pagb@ailleron.com>
2022-10-03 18:29:52 +0200

    Initial Commit

---
+++ Sample-SwiftUI-Task/Sample_SwiftUI_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SwiftUI_TaskApp.swift
+//  Sample-SwiftUI-Task
+//
+//  Created by Patryk Grabowski on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SwiftUI_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
